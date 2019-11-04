6b691ce4a800779d0b04f75e23b295396e322634
Iñaki Irujo Ganuza <inaki.irujo@gtei.es>
2019-11-04 10:32:38 +0100

    Proyecto muy avanzado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueRouter from 'vue-router'
+import router from './router'
+
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
